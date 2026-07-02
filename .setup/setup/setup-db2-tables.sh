ab4e507e5ac150e4b0e45acfbe9266a352ce91ae
David Meyer <dmbluesmith@gmail.com>
2026-07-02 15:01:17 -0400

    specifying db2 datasets

--- .setup/setup/setup-db2-tables.sh
+++ .setup/setup/setup-db2-tables.sh
@@ -19,6 +19,7 @@ source "$SCRIPTS_DIR/../config/setenv.sh"
 # Environment
 # =========================
 export ZOAU_HOME=$(get_section_value 'zoau' 'zoau_home')
+export CONFIG_FILE="$SCRIPTS_DIR/../config/config.yaml"
 
 export PATH="$ZOAU_HOME/bin:$PATH"
 export LIBPATH="$ZOAU_HOME/lib:${LIBPATH:-}"
@@ -28,9 +29,28 @@ export LIBPATH="$ZOAU_HOME/lib:${LIBPATH:-}"
 # =========================
 rm -f "/tmp/IMS-Db2-*"
 rm -f "/tmp/Db2-*"
-run_job_and_wait "$SCRIPTS_DIR/../jcl/cics/Db2-drop.jcl" "8"
-run_job_and_wait "$SCRIPTS_DIR/../jcl/cics/Db2-create.jcl"
+
+# Render CICS DB2 JCL templates
+python "$SCRIPTS_DIR/../lib/render_template.py" --configFile $CONFIG_FILE \
+    --extraVar "jobname=DB2DROP" --templateFile "$SCRIPTS_DIR/../jcl/cics/Db2-drop.j2" \
+    --outputFile "/tmp/Db2-drop-$$.jcl"
+
+python "$SCRIPTS_DIR/../lib/render_template.py" --configFile $CONFIG_FILE \
+    --extraVar "jobname=DB2CREAT" --templateFile "$SCRIPTS_DIR/../jcl/cics/Db2-create.j2" \
+    --outputFile "/tmp/Db2-create-$$.jcl"
+
+# Render IMS DB2 JCL templates
+python "$SCRIPTS_DIR/../lib/render_template.py" --configFile $CONFIG_FILE \
+    --extraVar "jobname=IMSDB2DR" --templateFile "$SCRIPTS_DIR/../jcl/ims/Db2-drop.j2" \
+    --outputFile "/tmp/IMS-Db2-drop-$$.jcl"
+
+python "$SCRIPTS_DIR/../lib/render_template.py" --configFile $CONFIG_FILE \
+    --extraVar "jobname=IMSDB2CR" --templateFile "$SCRIPTS_DIR/../jcl/ims/Db2-create.j2" \
+    --outputFile "/tmp/IMS-Db2-create-$$.jcl"
+
+run_job_and_wait "/tmp/Db2-drop-$$.jcl" "8"
+run_job_and_wait "/tmp/Db2-create-$$.jcl"
 # IMS DB2 setup
-jsub -f "$SCRIPTS_DIR/../jcl/ims/Db2-drop.jcl"
-jsub -f "$SCRIPTS_DIR/../jcl/ims/Db2-create.jcl"
+jsub -f "/tmp/IMS-Db2-drop-$$.jcl"
+jsub -f "/tmp/IMS-Db2-create-$$.jcl"
 exit $?
